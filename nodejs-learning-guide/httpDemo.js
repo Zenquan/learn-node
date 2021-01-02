2c200919c39bb7f596131cb83c3467c50eaddafb
zenquan <1847867150@qq.com>
2021-01-02 15:14:59 +0800

    feat: 更新代码

--- nodejs-learning-guide/httpDemo.js
+++ nodejs-learning-guide/httpDemo.js
@@ -1,21 +1,23 @@
 const http = require('http')
 
-let server = http.createServer()
-
-server.on("listening", () => {
-  console.log('111>>>');
+let server = http.createServer(() => {
+  console.log('app is running>>>');
 })
 
-server.on("connection", (socket) => {
-  socket.on('a', () => {
-    console.log('222>>>', 222);
-  })
-  console.log('111>>>');
-})
+// server.on("listening", () => {
+//   console.log('111>>>');
+// })
 
-server.on("connection", (socket) => {
-  socket.emit('a')
-  console.log('111>>>');
-})
+// server.on("connection", (socket) => {
+//   socket.on('a', () => {
+//     console.log('222>>>', 222);
+//   })
+//   console.log('111>>>');
+// })
+
+// server.on("connection", (socket) => {
+//   socket.emit('a')
+//   console.log('111>>>');
+// })
 
 server.listen(3200)
---
+++ nodejs-learning-guide/zen-koa-router.js
@@ -0,0 +1,20 @@
+// const Router = require('koa-router')
+// let page = new Router()
+// page.get('/404', (ctx)=>{
+//     ctx.body = '404 page!'
+// })
+// page.get('/hello', (ctx)=>{
+//     ctx.body = 'hello page!'
+// })
+
+// let router = new Router()
+// page.use('/', home.routes(), home.allowedMethods())
+// page.use('/page', page.routes(), page.allowedMethods())
+const http = require('http')
+
+class Router {
+
+};
+
+
+module.exports = Router;
